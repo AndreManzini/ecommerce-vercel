46bd1c6c731d7731c86e9cda3f41a118aec55b0c
Andre Mori <manzini.mori@gmail.com>
2023-07-19 08:15:58 -0600

    API

---
+++ src/routes/products/+page.server.js
@@ -0,0 +1,8 @@
+export async function load(event) {
+    const { fetch } = event;
+    const response = await fetch('/api/products');
+    const products = await response.json();
+    return {
+        products,
+    }
+}
